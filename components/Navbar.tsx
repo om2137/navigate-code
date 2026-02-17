af8004e68a3ba42782c4a4002141665d45a98511
om2137 <omraut961@gmail.com>
2026-02-17 17:05:32 +0530

    navbar component

---
+++ components/Navbar.tsx
@@ -0,0 +1,23 @@
+"use client"
+import Link from "next/link"
+
+export default function Navbar() {
+
+  return (
+    <header className="sticky top-0 z-50 bg-white border-b">
+      <div className="flex items-center justify-between h-14 px-6">
+        {/* <nav className="h-14.5 border-b flex items-center px-6 justify-between"> */}
+          <div className="font-semibold">CodeIntel</div>
+
+          <div className="flex gap-6 text-sm">
+            <Link href="/">Home</Link>
+            <Link href="/repos">Repositories</Link>
+            <Link href="/upload">Upload</Link>
+            <Link href="/health">Health</Link>
+          </div>
+        {/* </nav> */}
+
+      </div>
+    </header>
+  )
+}
